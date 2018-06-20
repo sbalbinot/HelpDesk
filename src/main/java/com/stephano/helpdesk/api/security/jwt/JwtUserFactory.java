4d85f098540338d885ece31d303e4255e1d6a962
stephano <stephanobalbinot@hotmail.com>
2018-06-20 19:51:24 -0300

    Autenticação e autorização com tokens JWT

---
+++ src/main/java/com/stephano/helpdesk/api/security/jwt/JwtUserFactory.java
@@ -0,0 +1,32 @@
+package com.stephano.helpdesk.api.security.jwt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.stephano.helpdesk.api.entity.User;
+import com.stephano.helpdesk.api.enums.ProfileEnum;
+
+public class JwtUserFactory {
+	
+	private JwtUserFactory() {
+    }
+
+    public static JwtUser create(User user) {
+        return new JwtUser(
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                mapToGrantedAuthorities(user.getProfile())
+        );
+    }
+
+    private static List<GrantedAuthority> mapToGrantedAuthorities(ProfileEnum profileEnum) {
+    		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>(); 
+    		authorities.add(new SimpleGrantedAuthority(profileEnum.toString())); 
+    		return   authorities ;
+    }
+	
+}
